f13c16b477793e5f827831222cc74926b92081e6
Manuel Alejandro Zeña Reque <mzenareq@emeal.nttdata.com>
2022-02-24 22:12:54 -0500

    first commit

---
+++ src/main/java/com/nttdata/BootCampProyectoIG6/BootCampProyectoIg6Application.java
@@ -0,0 +1,13 @@
+package com.nttdata.BootCampProyectoIG6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampProyectoIg6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCampProyectoIg6Application.class, args);
+	}
+
+}
